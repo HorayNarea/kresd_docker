50fe289cf46779a90794f0400ba496bb37d34b4d
Thomas Sänger <thomas@gecko.space>
2021-04-12 04:54:42 +0200

    do healthcheck first

--- check_and_gc.sh
+++ check_and_gc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+# do a DNS lookup to check health of kresd
+dig @127.0.0.1 || exit 1
+
 # run cache garbage collector
 kres-cache-gc -c /data
-
-# do a DNS lookup to check health of kresd
-dig @127.0.0.1
